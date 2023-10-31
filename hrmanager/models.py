722433a44e505bbe40b2e118d50661d8fc814b3f
Cyril-CRGB <>
2023-10-31 07:24:14 +0000

    building model salary_input, meta class, def str, makemigrations and migrate

--- hrmanager/models.py
+++ hrmanager/models.py
@@ -3,51 +3,102 @@ from django.contrib.auth.models import User
 from cloudinary.models import CloudinaryField
 
 
-STATUS = ((0, "active"), (1, "inactive"))
+STATUS = ((0, "Active"), (1, "Inactive"))
+EMPLOYEES_GENDER = [
+    ("F", "Female"),
+    ("M", "Male"),
+    ("O", "Other"),
+]
+EMPLOYEES_MARITAL_STATUS = [
+    ("M", "Married"),
+    ("D", "Divorced"),
+    ("S", "Single"),
+    ("P", "Partnership"),
+    ("O", "Other"),
+]
+CHILDREN_BELOW_18 = [
+    ("ONE", 1),
+    ("TWO", 2),
+    ("THREE", 3),
+    ("FOUR", 4),
+    ("FIVE", 5),
+    ("SIX", 6),
+    ("SEVEN", 7),
+    ("EIGHT", 8),
+    ("NINE", 9),
+    ("THEN", 10),
+]
+CHILDREN_BELOW_25 = [
+    ("ONE", 1),
+    ("TWO", 2),
+    ("THREE", 3),
+    ("FOUR", 4),
+    ("FIVE", 5),
+    ("SIX", 6),
+    ("SEVEN", 7),
+    ("EIGHT", 8),
+    ("NINE", 9),
+    ("THEN", 10),
+]
 
 
 class Employees(models.Model):
+    # basic personal information
     first_name = models.CharField(max_length=30)
     last_name = models.CharField(max_length=30)
-
-    EMPLOYEES_GENDER = [
-        ("F", "Female"),
-        ("M", "Male"),
-        ("O", "Other"),
-    ]
     employees_gender = models.CharField(max_length=1, choices=EMPLOYEES_GENDER)
-
-    EMPLOYEES_MARITAL_STATUS = [
-        ("M", "Married"),
-        ("D", "Divorced"),
-        ("S", "Single"),
-        ("P", "Partnership"),
-        ("O", "Other"),
-    ]
     employees_marital_status = models.CharField(
         max_length=1, choices=EMPLOYEES_MARITAL_STATUS)
-
-    ChildrenBelow18 = models.IntegerChoices(
-        "Number of children below 18", 0 1 2 3 4 5 6 7 8 9 10)
     children_for_allocations_type_1 = models.IntegerField(
-        choices=ChildrenBelow18.choices)
-    ChildrenBelow25 = models.IntegerChoices(
-        "Number of children below 25 still studying", 0 1 2 3 4 5 6 7 8 9 10)
+        choices=CHILDREN_BELOW_18)
     children_for_allocations_type_2 = models.IntegerField(
-        choices=ChildrenBelow25.choices)
-
-    start_date = models.DateField()
-    end_date = models.DateField()
+        choices=CHILDREN_BELOW_25)
     birth_date = models.DateField()
     employees_age = models.PositiveSmallIntegerField()
     email_adress = models.EmailField()
     phone_number = models.IntegerField()
-    emergency_contact = models.CharField()
+    emergency_contact = models.CharField(max_length=30)
     emergency_phonenumber = models.IntegerField()
-    employee_picture = models.ImageField()
-    social_security_number = models.PositiveBigIntegerField()
+    employee_picture = CloudinaryField('image', default='placeholder')
+    social_security_number = models.CharField(
+        max_length=13, unique=True)
+    employees_bankaccount = models.CharField(max_length=21)
+    # basic salary information
+    start_date = models.DateField()
+    end_date = models.DateField()
+    employees_holiday_rights = models.IntegerField()
     base_monthly_salary = models.IntegerField()
-
-    created_date = models.DateField(auto_now_add=True)
-    updated_date = models.DateField(auto_now=True)
+    employees_phone_allocation = models.PositiveSmallIntegerField()
+    employees_representation_allocation = models.PositiveSmallIntegerField()
+    # basic calculated information
+    created_date = models.DateTimeField(auto_now_add=True)
+    updated_date = models.DateTimeField(auto_now=True)
     seniority = models.DurationField()
+    # status
+    employees_status = models.IntegerField(choices=STATUS, default=0)
+
+    class Meta:
+        ordering = ['start_date']
+        unique_together = [['first_name', 'last_name']]
+
+    def __str__(self):
+        return self.title
+
+
+class salary_items(models.Model):
+    # constant variables for automatic calculation
+    avs_item = models.DecimalField(max_digits=4, decimal_places=2)
+    ac_item = models.DecimalField(max_digits=4, decimal_places=2)
+    ac2_item = models.DecimalField(max_digits=4, decimal_places=2)
+    laap_item = models.DecimalField(max_digits=4, decimal_places=2)
+    laanp_item = models.DecimalField(max_digits=4, decimal_places=2)
+    laac_item = models.DecimalField(max_digits=4, decimal_places=2)
+    laace_item = models.DecimalField(max_digits=4, decimal_places=2)
+    amat_item = models.DecimalField(max_digits=4, decimal_places=2)
+    alfa_item = models.DecimalField(max_digits=4, decimal_places=2)
+    apgmal_item = models.DecimalField(max_digits=4, decimal_places=2)
+    alpetiteenfance_item = models.DecimalField(max_digits=4, decimal_places=2)
+    # temporary variables for manual entries
+    expense_report = models.DecimalField(max_digits=5, decimal_places=2)
+    public_transportation_fees = models.DecimalField(
+        max_digits=6, decimal_places=2)
