d0be6a0eb0a0a0bc31448e37d0bbe56bb14678e0
Cyril-CRGB <>
2023-11-04 07:05:47 +0000

    fixing bugs in index.html

--- hrmanager/views.py
+++ hrmanager/views.py
@@ -6,6 +6,6 @@ from .models import Employees
 class EmployeesList(generic.ListView):
     model = Employees
     queryset = Employees.objects.filter(
-        employees_status=0).order_by('-last_name')
+        employees_status=0).order_by('last_name')
     template_name = 'index.html'
     paginate_by = 6
