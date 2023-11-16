e375d0230a0919803977652ac99700111749dd58
Cyril-CRGB <>
2023-11-16 07:36:11 +0000

    Adding items to the form on employee_add.html, modifying view EnployeeAddView

--- hrmanager/views.py
+++ hrmanager/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.views import generic
 from .models import Employees
 from django.views import View
+from django.urls import reverse_lazy
 # from django import template
 
 # register = template.Library()
@@ -34,7 +35,13 @@ class EnployeeDetailView(generic.DetailView):
     context_object_name = 'selected_employee'
 
 
-class EnployeeAddView(generic.ListView):
+class EnployeeAddView(generic.edit.CreateView):
     model = Employees
     template_name = 'employee_add.html'
-    context_object_name = 'employee_add'
+    fields = ['first_name', 'last_name', 'employees_gender',
+              'employees_marital_status', 'children_for_allocations_type_1', 'children_for_allocations_type_2',
+              'birth_date', 'employees_age', 'email_adress', 'phone_number', 'emergency_contact', 'emergency_phonenumber',
+              'employee_picture', 'social_security_number', 'employees_bankaccount', 'start_date', 'end_date',
+              'employees_holiday_rights', 'base_monthly_salary', 'employees_phone_allocation',
+              'employees_representation_allocation', 'seniority', 'employees_status']
+    success_url = reverse_lazy('Listofemployees')
