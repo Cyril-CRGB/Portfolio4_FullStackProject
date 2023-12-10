1c0d0a7aac2590dda843173f79ae4bd69b09a819
Cyril-CRGB <>
2023-12-10 19:40:02 +0000

    Readme file basic infos

--- octopus/settings.py
+++ octopus/settings.py
@@ -41,7 +41,7 @@ X_FRAME_OPTIONS = 'SAMEORIGIN'
 #    ALLOWED_HOSTS = [
 #        'portfolio4-fullstackproject-67b0ea26bc65.herokuapp.com']
 
-ALLOWED_HOSTS = ['portfolio4-fullstackproject-67b0ea26bc65.herokuapp.com']
+ALLOWED_HOSTS = ['8000-cyril-crgb-portfolio4-fu-5iyjbxlegt.us2.codeanyapp.com']
 
 # Application definition
 
