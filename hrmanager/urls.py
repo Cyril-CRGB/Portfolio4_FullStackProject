a9415ed50022123da0b8429e955a3d4c007129db
Cyril-CRGB brom <crg.bromberger@gmail.com>
2024-10-16 23:16:43 +0000

    Add: authentication tag

--- hrmanager/urls.py
+++ hrmanager/urls.py
@@ -3,10 +3,8 @@ from django.urls import path
 
 
 urlpatterns = [
-    # path('', HomeView, name='home')
     path('', views.HomeView.as_view(), name='home'),
     path('employees/', views.EmployeesList.as_view(), name='Listofemployees'),
-    # path('employeesdetail/', views.EmployeesDetail.as_view(), name='Detailofemployees'),
     path('employee/<int:pk>/', views.EnployeeDetailView.as_view(),
          name='Detailofemployees'),
     path('employee/new/', views.EnployeeAddView.as_view(),
